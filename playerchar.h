5a51ce48e197a0b1b92dc2a44e9ec986555246e0
Rafy Bhuiyan <rafybhuiyan55@gmail.com>
2024-01-11 04:45:45 +0600

    Update playerchar.h

--- playerchar.h
+++ playerchar.h
@@ -1,5 +1,6 @@
 char player_attack[5][40] = { "image/Shinobi/Attack/Attack_01.png", "image/Shinobi/Attack/Attack_02.png", "image/Shinobi/Attack/Attack_03.png", "image/Shinobi/Attack/Attack_04.png", "image/Shinobi/Attack/Attack_05.png" };
 char player_jump[8][40] = { "image/Shinobi/Jump/Jump_01.png", "image/Shinobi/Jump/Jump_02.png", "image/Shinobi/Jump/Jump_03.png", "image/Shinobi/Jump/Jump_04.png", "image/Shinobi/Jump/Jump_05.png", "image/Shinobi/Jump/Jump_07.png","image/shinobi/Jump/Jump_08.png","image/shinobi/Jump/Jump_09.png"};
 char player_run[3][40] = { "image/Shinobi/Run/Run_01.png", "image/Shinobi/Run/Run_02.png", "image/Shinobi/Run/Run_03.png" };
+char player_revrun[3][40] = { "image/Shinobi/Run/Run_01.png", "image/Shinobi/Run/Run_02.png", "image/Shinobi/Run/Run_03.png" };
 char player_idle[40] = { "image/Shinobi/Idle/Idle_01.png"};
-char player_sheild[3][40] = { "", "", "" };
+char player_sheild[3][40] = { "image/Shinobi/Shield/Shield_01.png", "image/Shinobi/Shield/Shield_02.png", "image/Shinobi/Shield/Shield_03.png" };
